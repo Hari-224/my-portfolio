65713cc495a29270599e2899020bc45626c894e9
Hari-224 <727823tucs105@skct.edu.in>
2025-11-01 21:17:22 +0530

    Initial commit

--- src/components/common/CustomCursor/CustomCursor.jsx
+++ src/components/common/CustomCursor/CustomCursor.jsx
@@ -1,20 +1,4 @@
-// // components/common/CustomCursor/CustomCursor.jsx
-// import React from 'react';
-// import './CustomCursor.css';
 
-// const CustomCursor = ({ position }) => {
-//   return (
-//     <div 
-//       className="custom-cursor"
-//       style={{
-//         left: position.x - 10,
-//         top: position.y - 10
-//       }}
-//     />
-//   );
-// };
-
-// export default CustomCursor;
 import React from 'react';
 import './CustomCursor.css';
 
